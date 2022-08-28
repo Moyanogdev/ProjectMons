fd40917f1a740ac7bee43208c3b0f5815f7a2403
Gonzalo Moyano <gmoyanodvlpr@gmail.com>
2022-08-27 21:08:44 -0300

    Segunda etapa del proyecto. Seccion Sobre Nos & JS

--- main.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener("click", function (e){
-    if(e.target.classList.contains("burguerProductos")){
-        const src = e.target.getAttribute("src");
-        document.querySelector(".modal-img").src = src;
-        const myModal = new bootstrap.Modal(document.getElementById('productosModal'))
-        myModal.show();
-    }
-})
